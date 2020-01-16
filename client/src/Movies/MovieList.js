a45e296508962f7f9a65655cbc8c117d72b58bce
rdowd <rjarrettdowd@yahoo.com>
2020-01-15 20:16:55 -0500

    Home button in SavedList component now works.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -21,20 +21,22 @@ const MovieList = props => {
   
   return (
     <div className="movie-list">
-      {movies.map(movie => (
-        <Link to={`/movies/${movie.id}`}>
-          <MovieDetails key={movie.id} movie={movie} />
-        </Link>
+      {movies.map(movie => (       
+        <MovieDetails key={movie.id} movie={movie} /> 
       ))}
     </div>
   );
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  const { title, director, metascore, stars, id } = movie;
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
+      
+      <Link to={`/movies/${id}` }>
+        <h2>{title}</h2>
+      </Link>
+      
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
